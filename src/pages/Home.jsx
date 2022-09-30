bd9268d09683b7ad7db5396230a6bf6954565ef2
Stark Programmer <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-10-01 00:02:57 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
+
+// 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
